7a2cde90ac35a51bd906af737da3a3ef9f4bc4d5
Jerrod <jerrod@kernelle.net>
2026-01-28 15:59:18 -0700

    Fixing a few bugs

---
+++ scripts/development/bump_cargo_version.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+set -euo pipefail
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+USAGE="$(cat <<EOF
+Pulls the latest version of the repository from git and applies it to the Cargo workspace version.
+
+Usage: bump_cargo_version.sh
+
+Flags:
+	-h,--help		Show this help text
+EOF
+)"
+
+source "$REPO_ROOT/scripts/shared/log.func.sh"
+source "$REPO_ROOT/scripts/shared/get_current_version.func.sh"
+
+PATH_CARGO_WORKSPACE="$REPO_ROOT/crates/Cargo.toml"
+
+function main() {
+	local current_version old_version_line new_version_line
+
+	parse_args "$@"
+
+	current_version="$(get_current_version)"
+	new_version_line="$(format_version_line "$current_version")"
+	old_version_line="$(get_cargo_workspace_version)"
+	
+	if [[ "$new_version_line" = "$old_version_line" ]]; then
+		log "Cargo workspace is up-to-date with latest version '$current_version'"
+		exit 0
+	fi
+
+	replace_workspace_version "$old_version_line" "$new_version_line"
+	log "Successfully updated Cargo.toml to '$current_version'"
+}
+
+: <<'DOC'
+	Parses CLI flags. 
+	See USAGE for flag descriptions.
+DOC
+function parse_args() {
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-h|--help)	log "$USAGE" && exit 0;;
+			*)
+				log "Unknown option: $1"
+				log "$USAGE"
+				exit 1
+				;;
+		esac
+		shift
+	done
+}
+
+: <<'DOC'
+Gets a clean semver string from the workspace Cargo.toml file.
+
+This includes stripping any quote literals and leading characters, 
+returning just the version itself.
+
+If none is specified, defaults to "0.0.0"
+DOC
+function get_cargo_workspace_version() {
+	local version="" 
+	local found_workspace_package=false
+
+	# Walks through the Cargo.toml line by line
+	while IFS= read -r line; do
+		if [[ "$line" == "[workspace.package]" ]]; then
+			found_workspace_package=true
+		
+		elif [[ "$line" == "["* ]]; then
+			found_workspace_package=false
+		
+		elif $found_workspace_package && [[ "$line" == version* ]]; then
+			version="$line"
+			break
+		
+		fi
+	done < "$PATH_CARGO_WORKSPACE"
+
+	if [[ -n "$version" ]]; then
+		echo "$version"
+	else
+		format_version_line "0.0.0"
+	fi
+}
+
+function format_version_line() {
+	echo "version = \"$1\""
+}
+
+function replace_workspace_version() {
+	local previous_version next_version
+
+	previous_version="$1"
+	next_version="$2"
+
+	sed -i "s/$previous_version/$next_version/" "$PATH_CARGO_WORKSPACE"
+}
+
+main "$@"
---
+++ scripts/shared/get_current_version.func.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+: <<'DOC'
+	Gets the latest tagged version from git. This can be either a pre-release or full release.
+
+	If there is no previous tag, this returns "0.0.0". Otherwise, it will get the current tag
+	with the leading 'v' stripped.
+DOC
+function get_current_version() {
+	local latest_tag semver
+
+	latest_tag="$(git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")"
+
+	# Strip leading 'v' if there is one
+	semver="${latest_tag#v}"
+	echo "$semver"
+}
