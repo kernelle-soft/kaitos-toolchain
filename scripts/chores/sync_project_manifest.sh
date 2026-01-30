8df69194818d855c0d94c97045c17d70cc48d4c1
Jerrod <jerrod@kernelle.net>
2026-01-30 11:55:54 -0700

    Using different version semantics for project.json

--- scripts/chores/sync_project_manifest.sh
+++ scripts/chores/sync_project_manifest.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 USAGE="$(cat <<EOF
-Syncs the project version to the git version
+Syncs up the project manifest (project.json) based on the latest in git.
+
+Usage: sync_project_manifest.sh
 
 Flags:
   -h, --help      Show this help text.
@@ -14,17 +16,21 @@ EOF
 source "$REPO_ROOT/scripts/shared/log.func.sh"
 source "$REPO_ROOT/scripts/shared/git/latest_version.func.sh"
 source "$REPO_ROOT/scripts/shared/git/parse_version.func.sh"
+source "$REPO_ROOT/scripts/shared/git/compare_versions.func.sh"
 
 PROJECT_MANIFEST="$REPO_ROOT/project.json"
 
 function main() {
-  local git_version updated_latest_version updated_latest_release
+  local git_version manifest_latest updated_latest_release
+  local releases
 
   parse_args "$@"
   
   git_version="$(latest_version)"
-  updated_latest_version="$(plan_latest_version)"
-  updated_latest_release="$(plan_latest_release)"
+  manifest_latest="$(plan_latest_version)"
+  updated_latest_release="$(latest_release_version)"
+  releases="$(jq .successful_releases < "$PROJECT_MANIFEST")"
+
 }
 
 : <<'DOC'
@@ -45,6 +51,10 @@ function parse_args() {
   done
 }
 
+function parse_meta() {
+  local -n name latest_version latest_release successful_releases series
+}
+
 function get_release_name() {
   if is_before_v1; then
     get_pokemon_name
@@ -55,7 +65,7 @@ function get_release_name() {
 }
 
 function is_before_v1() {
-  jq -e '.version | startswith("0.")' "$PROJECT_MANIFEST" > /dev/null
+  jq -e '.stable | startswith("0.")' "$PROJECT_MANIFEST" > /dev/null
 }
 
 function get_pokemon_name() {
@@ -69,25 +79,17 @@ function get_pokemon_name() {
 }
 
 function plan_latest_version_update() {
-  local latest_version git_version
-  git_version="$(latest_version)"
-  latest_version="$(jq .latest_version < "$PROJECT_MANIFEST")"
+  local manifest_latest git_latest result
+  git_latest="$(latest_version)"
+  manifest_latest="$(jq .latest < "$PROJECT_MANIFEST")"
 
-  # If git version is latest, then update latest version
-  # TODO: use new git comparison function to determine version supremacy
-  if [[ ]]
-}
+  # If git version is latest, then update latest version  
+  result="$(compare_versions "$git_latest" "$manifest_latest")"
+  if [[ "$result" = -1 ]]; then
+    manifest_latest="$git_latest"
+  fi
 
-function plan_latest_release_update() {
-  local git_version latest_release git_release
-  git_version="$(latest_version)"
-  git_release="$(latest_release_version)"
-  project_latest_release="$(jq .latest_release < "$PROJECT_MANIFEST")"
-
-  # If the newest git version isn't pre-release
-
+  echo "$manifest_latest"
 }
 
 main "$@"
-
-#TODO: Pokemonify
