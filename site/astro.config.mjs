04f74f2197d0e4b550d838219ac3c98269af4859
Jerrod <jerrod@kernelle.net>
2026-02-17 22:03:03 -0700

    Setting up basic docsite (#46)
    * adding to gitignore
    
    * pinning node 25.6.1
    
    * create template docsite
    
    * Adding cleanup for docs
    
    * updating just recipes
    
    * update repo link
    
    * clean up astro config
    
    * Set site package to private
    
    * Use LTS node version

---
+++ site/astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Kaitos Toolchain',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/kernelle-soft/kaitos-toolchain'
+        }
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    })
+  ],
+});
