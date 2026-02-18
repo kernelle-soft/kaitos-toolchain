27419ae9070d3270503b1a70d020eb9dee51eba1
Jerrod <jerrod@kernelle.net>
2026-02-17 23:17:46 -0700

    Add site to astro config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kaitos.dev',
   integrations: [
     starlight({
       title: 'Kaitos Toolchain',
