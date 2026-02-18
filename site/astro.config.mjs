11a0f9912dc668a1f4753cb306342a92843f0fa9
Jerrod <jerrod@kernelle.net>
2026-02-17 21:38:33 -0700

    clean up astro config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,25 +2,31 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import mdx from '@astrojs/mdx';
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'Kaitos Toolchain',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kernelle-soft/kaitos-toolchain' }],
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-		}), mdx()],
+  integrations: [
+    starlight({
+      title: 'Kaitos Toolchain',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/kernelle-soft/kaitos-toolchain'
+        }
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    })
+  ],
 });
