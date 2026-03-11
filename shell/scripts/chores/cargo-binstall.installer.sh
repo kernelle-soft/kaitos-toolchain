f601f3ee8e2a9426639cb656e71ea0d74eff30e8
Jerrod <jerrod@kernelle-soft.com>
2026-03-11 02:55:55 -0600

    install binstall from curl instead of compiling

---
+++ shell/scripts/chores/cargo-binstall.installer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+curl -L --proto '=https' --tlsv1.2 -sSf \
+  https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
+  | bash
